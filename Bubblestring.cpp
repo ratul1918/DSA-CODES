d73e18268fa8f698413bdb2fdb3bcdb2c51cb3bb
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-27 04:24:55 +0600

    Update Bubblestring.cpp

--- Bubblestring.cpp
+++ Bubblestring.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 using namespace std;
 
+// Function to perform Bubble Sort on a string array
 void BubbleSort(string arr[], int n) {
     string temp;
+    // Outer loop for passes
     for(int i = 0; i < n - 1; i++) {
+        // Inner loop for comparing adjacent elements
         for(int j = 0; j < n - 1 - i; j++) {
+            // Swap if strings are in the wrong order
             if(arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -14,6 +18,7 @@ void BubbleSort(string arr[], int n) {
     }
 }
 
+// Function to print the array of strings
 void printArray(string arr[], int n) {
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
@@ -22,17 +27,24 @@ void printArray(string arr[], int n) {
 
 int main() {
     int n;
+
+    // Ask user for the number of strings
     cout << "Enter the number of strings: ";
     cin >> n;
 
+    // Declare array of strings
     string arr[n];
+
+    // Input strings from user
     cout << "Enter the strings:\n";
     for (int i = 0; i < n; ++i) {
         cin >> arr[i];
     }
 
+    // Sort the strings using Bubble Sort
     BubbleSort(arr, n);
 
+    // Display the sorted strings
     cout << "\nSorted strings:\n";
     for (int i = 0; i < n; ++i) {
         cout << arr[i] << "\n";
