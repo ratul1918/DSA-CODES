488dfdf6fc4e76fe99a25360a9352b8273ea8dbd
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-28 22:05:31 +0600

    Update primenumber.cpp

--- primenumber.cpp
+++ primenumber.cpp
@@ -1,25 +1,32 @@
-#include<iostream>
+#include<iostream> // Include standard input-output library
 using namespace std;
 
 int main(){
     int n;
-    bool isPrime=true;
-    cout<<"enter testing number: ";
-    cin>>n;
+    bool isPrime = true; // Assume the number is prime initially
 
-    for(int i=2; i*i <= n-1 ;i++){
-        if(n%i == 0){
-            isPrime=false;
-            break;
+    cout << "Enter testing number: "; // Ask user for input
+    cin >> n; // Read the input number
+
+    // Prime numbers are greater than 1
+    if (n <= 1) {
+        isPrime = false;
+    } else {
+        // Loop from 2 to sqrt(n) to check if n is divisible by any number
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                isPrime = false; // Found a divisor, so not prime
+                break; // No need to check further
+            }
         }
-
     }
 
-    if(isPrime== true){
-        cout<<"number is prime";
-    }
-    else{
-        cout<<"number is not prime";
+    // Print the result based on isPrime flag
+    if (isPrime) {
+        cout << "Number is prime";
+    } else {
+        cout << "Number is not prime";
     }
 
+    return 0;
 }
