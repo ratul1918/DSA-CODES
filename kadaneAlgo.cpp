29a4e2c0b22520d39c3c12655b90a5dd88a72855
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-30 04:30:14 +0600

    Add files via upload

---
+++ kadaneAlgo.cpp
@@ -0,0 +1,21 @@
+// Problem: Find the contiguous subarray with the maximum sum
+// Given an integer array nums, find the contiguous subarray with the largest sum.
+
+#include <iostream>
+using namespace std;
+
+int maxSubArray(int arr[], int n) {
+    int maxSum = arr[0], currSum = arr[0];
+    for (int i = 1; i < n; i++) {
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSum = max(maxSum, currSum);
+    }
+    return maxSum;
+}
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "Maximum Subarray Sum: " << maxSubArray(arr, n);
+    return 0;
+}
