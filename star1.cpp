51af50f82535d651fce654aa95930a7654bd8a7f
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-06-18 02:55:57 +0600

    Update star1.cpp

--- star1.cpp
+++ star1.cpp
@@ -6,11 +6,11 @@ int main(){
     cout << "Enter : ";
     cin >> n;
 
-    // Outer loop controls the number of rows
+    // Outer loop controls the number of rows..
     for(int i = 0; i < n; i++){
-        char ch = 'A';  // Start each row with character 'A'
+        char ch = 'A';  // Start each row with character 'A'..
 
-        // Inner loop prints characters from 'A' to the nth alphabet
+        // Inner loop prints characters from 'A' to the nth alphabet..
         for(int j = 0; j < n; j++){
             cout << ch;   // Print the current character
             ch = ch + 1;  // Move to the next character
