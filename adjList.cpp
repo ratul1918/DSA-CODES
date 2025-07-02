bd30d182b8228d27598ecda62bb349183566d2e5
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-07-03 02:05:14 +0600

    Add files via upload

--- adjList.cpp
+++ adjList.cpp
@@ -1,48 +1,28 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-class Graph {
-private:
-    int V;
-    vector<vector<int> > adjList;
-public:
-    Graph(int v) {
-        this->V = v;
-        adjList.resize(V);
-    }
-
-    void addEdge(int u, int v) {
-        if (u >= 0 && u < V && v >= 0 && v < V) {
-            adjList[u].push_back(v);
-            adjList[v].push_back(u); // For undirected graph...
-        }
-        else {
-            cout << "Out of boundary!" << endl;
-        }
-    }
-
-    void printGraph() {
-        for (int i = 0; i < V; i++) {
-            cout << "Vertex " << i << ": ";
-            for (int j : adjList[i]) {
-                cout << j << " ";
-            }
-            cout << endl;
-        }
-    }
-};
 int main() {
-    int V = 4;
-    Graph g(V);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 3);
+    int vertex, edges;
+    cin >> vertex >> edges; // Input number of vertices and edges
 
-    cout << "Graph adjacency list:" << endl;
-    g.printGraph();
+    vector<int> adList[100]; // Adjacency list (array of vectors)
+
+    int u, v;
+    for (int i = 0; i < edges; i++) {
+        cin >> u >> v;              // Input one edge
+        adList[u].push_back(v);     // Add edge u -> v
+        adList[v].push_back(u);     // Add edge v -> u (undirected)
+    }
+
+    // Print adjacency list
+    for (int i = 0; i < vertex; i++) {
+        cout << i << " -> ";
+        for (int j = 0; j < adList[i].size(); j++) {
+            cout << adList[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
