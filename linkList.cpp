9691612b9fd0e832fc255fb18e3c53d792c3ab79
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-08-09 03:08:53 +0600

    Update linkList.cpp

--- linkList.cpp
+++ linkList.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a node in the doubly linked list.
+// Define the structure for a node in the doubly linked list
 typedef struct Node {
     int data;
     struct Node* prev; // Pointer to previous node.
