e1212264909f49891c399e54a778d1c6e5ee9e0f
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-09-28 04:07:39 +0600

    Add files via upload

---
+++ commonSubsequence.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+// Function to find the length of the longest common subsequence of
+// sequences `X[0…m-1]` and `Y[0…n-1]`
+int LCSLength(string X, string Y, int m, int n)
+{
+    // return if the end of either sequence is reached
+    if (m == 0 || n == 0) {
+        return 0;
+    }
+ 
+    // if the last character of `X` and `Y` matches
+    if (X[m - 1] == Y[n - 1]) {
+        return LCSLength(X, Y, m - 1, n - 1) + 1;
+    }
+ 
+    // otherwise, if the last character of `X` and `Y` don't match
+    return max(LCSLength(X, Y, m, n - 1), LCSLength(X, Y, m - 1, n));
+}
+ 
+int main()
+{
+    string X, Y;
+    
+    cout << "Enter first string: ";
+    cin >> X;
+    cout << "Enter second string: ";
+    cin >> Y;
+ 
+    cout << "The length of the LCS is " << 
+            LCSLength(X, Y, X.length(), Y.length()) << endl;
+ 
+    return 0;
+}
